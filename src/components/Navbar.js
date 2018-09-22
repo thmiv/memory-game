3b786845ef6647cf69022cd95208754ab75cbc59
thmiv <thmiv4@gmail.com>
2018-09-22 17:16:03 -0500

    working build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,13 @@
 import React from "react";
 import "../styles/Navbar.css";
 
-const styles = {
-  navTextStyle: {
-    float: "right"
-  }
-};
-
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg">
     <span>
       <a className="navbar-brand" href="">React Says</a>
     </span>
-
-    <span style={styles.navTextStyle} className="navbar-text">
-      Score: 0 | Top Score: 0
-    </span>
+    
   </nav>
 );
 
-export default Navbar;
+export default Navbar; 
--- src/components/Section.js
+++ src/components/Section.js
@@ -6,50 +6,67 @@ import friends from "./game-cards/gameIcons.json";
 class Section extends React.Component {
   state = {
     friends,
-    counter: 0
+    counter: 0,
+    topScore: 0
   };
 
   componentDidMount() {
-    console.log("component mounted");
     this.shuffleArray(friends);
   }
 
   shuffleArray = (friends) => {
     for (let i = friends.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [friends[i], friends[j]] = [friends[j], friends[i]]; // eslint-disable-line no-param-reassign
+      const j = Math.floor(Math.random() * (i + 1));
+      [friends[i], friends[j]] = [friends[j], friends[i]];
     }
     return this.setState({ friends })
-}
+  }
 
-  handleBtnClick = event => {
-    console.log(this.state.counter);
-    const btnType = event.target.attributes.getNamedItem("alt").value;
-    const itemIndex = event.target.attributes.getNamedItem("id").value -1;
+  checkGameStatus = (currentCard) => {
+    this.state.friends.map(friend => {
+      if (friend.name === currentCard) {
+        if (friend.beenClicked !== true) {
+          friend.beenClicked = true;
+          return this.setState({ counter: this.state.counter + 1 });
+        } else {
+          let tempScore = this.state.counter;
+          if (tempScore < this.state.topScore) {
+            tempScore = this.state.topScore;
+          }
 
-
-    friends.forEach(function(ele){
-      
-      if (ele.name === btnType) {
-        ele.beenClicked = true;
+          alert("YOU LOSE");
+          this.resetGame(friends);
+          return this.setState({
+            friends,
+            counter: 0,
+            topScore: tempScore
+          });
+        }
       }
     });
-    
-    console.log(btnType);
-    
-    this.setState({ 
-      friends,
-      counter: this.state.counter + 1
-     });
+  }
 
+  resetGame = (friends) => {
+    friends.forEach(function (friend) {
+      friend.beenClicked = false;
+    });
+    return this.setState({ friends });
+  }
+
+  handleBtnClick = event => {
+    const btnType = event.target.attributes.getNamedItem("alt").value;
+    this.checkGameStatus(btnType);
     this.shuffleArray(friends);
-    
-     console.log(this.state.friends);
+    //console.log("Score: " + this.state.counter, " | Top Score: " + this.state.topScore);
   };
 
   render() {
     return (
       <section className="container">
+      <div className="scoreHolder">
+      Score: <span className="scores">{this.state.counter}</span> | Top Score: <span className="scores">{this.state.topScore}</span>
+      </div>
+      <div className="cardBox">
         {this.state.friends.map(friend => (
           <FriendCard
             image={friend.image}
@@ -59,6 +76,7 @@ class Section extends React.Component {
             onClick={this.handleBtnClick}
           />
         ))}
+        </div>
       </section>
     )
   };
